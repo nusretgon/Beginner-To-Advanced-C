526f79282301749d421d1165fafcb63c1bc548ac
Nusret GON <72438433+nusretgon@users.noreply.github.com>
2022-05-18 19:29:25 +0300

    Add files via upload

---
+++ Refresher/1-) variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	
+	// C syntax is similar with Java.
+	int i=1;
+	float f=3.14;
+	double d= 82456;
+	char c ='C';
+	char str[]="Nusret";
+	printf(str);
+	return 0;
+	
+}
