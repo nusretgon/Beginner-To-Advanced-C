2519f80ff1d647b2d7e47e0ec8462808b9021de7
Nusret GON <72438433+nusretgon@users.noreply.github.com>
2022-05-24 00:14:46 +0300

    Add files via upload

---
+++ stack_test.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void main(){
+
+    stack_push(1);
+    stack_push(2);
+    stack_push(3);
+    stack_push(4);
+    print_stack();
+    printf("\n");
+    stack_pop();
+    print_stack();
+    printf("\n");
+    stack_pop();
+    print_stack();
+    printf("\n");
+    stack_pop();
+    print_stack();
+    printf("\n");
+    stack_pop();
+    print_stack();
+    printf("\n");
+    
+
+}
